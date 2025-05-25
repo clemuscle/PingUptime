f563ff7a2909cbdd181d7b06d96e58832c8edc73
clement <clementmoabenedetti@gmail.com>
2025-05-25 17:22:13 +0000

    fix(app) create_app directly in init

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
+# app/__init__.py
+
 from flask import Flask
+from .main import bp
 
 def create_app():
-    app = Flask(__name__)
-    from .main import bp
+    app = Flask(__name__, template_folder="template")
     app.register_blueprint(bp)
     return app
+
+# Expose l'instance pour Gunicorn
+app = create_app()
